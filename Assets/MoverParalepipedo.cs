6e6e5edb91a3a3890124526c3271d994d49a1cc8
unknown <franciscohernandez.22@hotmail.com>
2017-08-01 20:47:29 -0500

    Initial commit

---
+++ Assets/MoverParalepipedo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverParalepipedo : MonoBehaviour {
+
+    public float movementSpeed = 5f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0f, 1, 0f));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(new Vector3(0f, -1, 0f));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0f, 0f, 1) * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(0f, 0f, -1) * Time.deltaTime * movementSpeed);
+        }
+    }
+}
